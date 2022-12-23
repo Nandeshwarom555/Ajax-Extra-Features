172103fa91e50d32449d98b2ab8893838bbd4921
test00123 <109013696+test00123@users.noreply.github.com>
2022-12-23 16:29:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,10 +14,10 @@ from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from aiohttp import web
-from os import environ
+from os import environ 
+
 
 PORT = environ.get("PORT", "8080")
-
 class Bot(Client):
 
     def __init__(self):
@@ -48,14 +48,17 @@ class Bot(Client):
         await app.setup()
         bind_address = "0.0.0.0"
         await web.TCPSite(app, bind_address, PORT).start()
-    async def web_server():
-        web_app = web.Application(client_max_size=30000000)
-        web_app.add_routes(routes)
-        return web_app
-    routes = web.RouteTableDef()
-    @routes.get("/", allow_head=True)
-    async def root_route_handler(request):
-        return web.json_response("testbot")
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
+routes = web.RouteTableDef()
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("testbot")
+
 
     async def stop(self, *args):
         await super().stop()
